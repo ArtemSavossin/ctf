254d8586d6c33b5aee95b2aebf48c9a6acbdb102
Artyom Savossin <46690976+ArtemSavossin@users.noreply.github.com>
2021-10-02 21:56:00 +0300

    snake for hw

---
+++ snakeProtoPoll.js
@@ -0,0 +1,6 @@
+const frame = document.createElement('iframe');
+frame.src = 'https://c8y68.csb.app/';
+frmae.width = '800';
+frame.height = '800';
+alert('Come and play my snake!')
+document.body.appendChild(frame);
